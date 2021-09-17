25d964fe68f25e8f0661858dad989ca06d10ba15
cauliflowersingerbee <86426968+cauliflowersingerbee@users.noreply.github.com>
2021-09-17 09:24:56 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,38 +1,15 @@
 
 /*imports http module*/
-const http = require('http');
+const http = require('http'), 
+      fs = require('fs'), 
+      url = require('url');
 
 http.createServer((request, response) => {
-  response.writeHead(200, {'Content-Type': 'text/plain'});
-  response.end('Hello Node!\n');
-}).listen(8080);
+  let addr = request.url,
+  q = url.parse(addr, true),
+  filePath = '';
 
-console.log('My first Node test server is running on Port 8080.');
-
-
-/*imports URL module*/
-const url = require('url');
-let addr = 'http://localhost:8080/default.html?year=2017&month=february';
-let q = url.parse(addr, true);
-
-console.log(q.host); // returns 'localhost:8080'
-console.log(q.pathname); // returns '/default.html'
-console.log(q.search); // returns '?year=2017&month=february'
-
-let qdata = q.query; // returns an object: { year: 2017, month: 'february' }
-console.log(qdata.month); // returns 'february'
-
-/*parsing request.url for incoming requests*/
-const http = require('http'),
-        fs = require('fs'),
-       url = require('url');
-
-http.createServer((request, response) => {
-let addr = request.url,
-q = url.parse(addr, true),
-filePath = '';
-
-if (q.pathname.includes('documentation')) {
+  if (q.pathname.includes('documentation')) {
     filePath = (__dirname + '/documentation.html');
   } else {
     filePath = 'index.html';
@@ -53,4 +30,5 @@ if (q.pathname.includes('documentation')) {
 console.log('My test server is running on Port 8080.');
 
 
+ 
 
