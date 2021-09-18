91ff31871ea444066bdcf2ab2dd24633698d0164
cauliflowersingerbee <86426968+cauliflowersingerbee@users.noreply.github.com>
2021-09-18 23:56:19 +0200

    added array of top ten films

--- index.js
+++ index.js
@@ -1,2 +1,47 @@
 const express = require('express');
 const app = express();
+
+let topFilmss = [
+      {
+        title: 'Moonlight',
+        director: 'Barry Jenkins'
+      },
+      {
+        title: 'Hidden Figures',
+        director: 'Theodore Melfi'
+      },
+      {
+        title: 'Do the right thing',
+        director: 'Spike Lee'
+      },
+      {
+        title: 'Poetic Justice',
+        director: 'John Singleton'
+      },
+      {
+        title: 'Waiting to Exhale',
+        director: 'Forest Whitaker'
+      },
+      {
+        title: 'Daughters of the Dust',
+        director: 'Julie Dash'
+      },
+      {
+        title: 'Pariah',
+        director: 'Dee Rees'
+      },
+      {
+        title: 'Black Panther',
+        director: 'Ryan Coogler'
+      },
+      {
+        title: 'Beasts of the southern wild',
+        director: 'Benh Zeitlin'
+      },
+      {
+        title: 'Sorry to bother you',
+        director: 'Boots Riley'
+      }
+
+  ];
+  
