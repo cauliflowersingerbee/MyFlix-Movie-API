739a27830ed1439f3e2dd8a498cec4419629e5fe
cauliflowersingerbee <86426968+cauliflowersingerbee@users.noreply.github.com>
2021-09-19 10:31:37 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
+/*requiring express*/
 const express = require('express');
 const app = express();
 
+/*creating data about top 10 movies*/
 let topMovies = [
       {
         title: 'Moonlight',
@@ -45,14 +47,43 @@ let topMovies = [
 
   ];
   
-  // GET requests
+  //creating Express GET request to access movie data
   
   app.get('/movies', (req, res) => {
     res.json(topMovies);
   });
 
+  /*creating GET request at '/' endpoint to return
+  default textual response*/ 
   app.get('/', (req, res) => {
     res.send('Welcome to Kino Noir!');
   });
   
-  
+  /*using express.static to serve documentation.html
+  file from public folder*/
+  app.use(express.static('public'));
+
+  /*using Morgan to log all requests*/
+const morgan = require('morgan');
+
+app.use(morgan('common'));
+
+app.get('/', (req, res) => {
+  res.send('Welcome to Kino Noir!');
+});
+
+/*app.get('/secreturl', (req, res) => {
+  res.send('This is a secret url with super top-secret content.');
+});
+
+app.listen(8080, () => {
+  console.log('Your app is listening on port 8080.');
+});
+*/
+
+/*error-handling middleware function*/
+
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send('Yikes, something isn\'t quite right!');
+  });
