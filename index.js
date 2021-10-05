2cf3105b4caaff2fd300d234eebb3ed40aab501b
cauliflowersingerbee <86426968+cauliflowersingerbee@users.noreply.github.com>
2021-10-05 10:57:01 +0200

    Update index.js

--- index.js
+++ index.js
@@ -30,7 +30,22 @@ const app = express();
   app.use(router);
 
   const cors = require('cors');
+  //for all origins:
   app.use(cors());
+  /*For specific origins:
+  let allowedOrigins = ['http://localhost:8080', 'http://testsite.com'];
+
+app.use(cors({
+  origin: (origin, callback) => {
+    if(!origin) return callback(null, true);
+    if(allowedOrigins.indexOf(origin) === -1){ // If a specific origin isn’t found on the list of allowed origins
+      let message = 'The CORS policy for this application doesn’t allow access from origin ' + origin;
+      return callback(new Error(message ), false);
+    }
+    return callback(null, true);
+  }
+}));
+  */
 
   let auth = require('./auth')(app);
   const passport = require('passport');
