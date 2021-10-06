45d62a3026c602adda7eb663217d9f12c25b5bd5
cauliflowersingerbee <86426968+cauliflowersingerbee@users.noreply.github.com>
2021-10-06 09:24:42 +0200

    Update index.js

--- index.js
+++ index.js
@@ -172,11 +172,10 @@ app.put('/users/:Username', passport.authenticate('jwt', { session: false }), [
     return res.status(422).json({ errors: errors.array() });
   }
 
-  let hashedPassword = Users.hashPassword(req.body.Password);
   Users.findOneAndUpdate({ Username: req.params.Username }, { $set:
     {
       Username: req.body.Username,
-      Password: hashedPassword,
+      Password: req.body.Password,
       Email: req.body.Email,
       Birthday: req.body.Birthday
     }
