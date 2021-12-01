826e9a2ee18fba627595f693b598cd04a5cb47d5
cauliflowersingerbee <86426968+cauliflowersingerbee@users.noreply.github.com>
2021-12-01 11:52:10 +0100

    Update index.js

--- index.js
+++ index.js
@@ -148,7 +148,7 @@ app.post('/users', [
 
 
 
-app.put('/users/:Username', [
+app.put('/users/:Username', passport.authenticate('jwt', { session: false }), [
   check('Username', 'Username is required').isLength({min: 5}),
   check('Username', 'Username contains non alphanumeric characters - not allowed.').isAlphanumeric(),
   check('Password', 'Password is required').not().isEmpty(),
